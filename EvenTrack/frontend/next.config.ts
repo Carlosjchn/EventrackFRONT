0420dd16a5b93747d2782de0984013830506b518
Carlos <carlosjchamorro@gmail.com>
2024-12-03 11:31:02 +0100

    otra vez

--- EvenTrack/frontend/next.config.ts
+++ EvenTrack/frontend/next.config.ts
@@ -1,26 +1,18 @@
-import { createProxyMiddleware } from 'http-proxy-middleware';
+import type { NextConfig } from "next";
 
-const nextConfig = {
-  async rewrites() {
+const nextConfig: NextConfig = {
+  /* Other config options here */
+  
+  async headers() {
     return [
       {
-        source: '/api/:path*',
-        destination: 'http://alexcoding.es:3030/api/:path*', // Proxy to your HTTP backend
-      },
-    ];
-  },
-
-  // You can also add custom middleware for full control of the proxy
-  async serverMiddleware() {
-    // Proxy API requests
-    return [
-      {
-        path: '/api/*',
-        handler: createProxyMiddleware({
-          target: 'http://alexcoding.es:3030',
-          changeOrigin: true, // Ensure the origin is correctly set for the proxy
-          pathRewrite: { '^/api': '' }, // Optional: strip the '/api' prefix from the forwarded request
-        }),
+        source: "/(.*)", // Apply for all routes
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "default-src 'self'; script-src 'self'; style-src 'self'; connect-src 'self'  http://alexcoding.es:3030/api/; img-src 'self'; font-src 'self';", // Add your HTTP backend URL here
+          },
+        ],
       },
     ];
   },
